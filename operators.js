9803531fb9c5e83c5912d1c792c0cf7bdb4500d4
Harish-Kanagaraj <134590329+Harish-Kanagaraj@users.noreply.github.com>
2025-02-14 23:05:41 +0530

    Add files via upload
    Operators

---
+++ operators.js
@@ -0,0 +1,101 @@
+let x = 5;
+let y = 2;
+//Arthimatic Operator
+console.log(x+y);
+console.log(x-y);
+console.log(x*y);
+console.log(x/y);
+console.log(x%y);
+console.log(x>y);
+
+//Assignment Operator
+console.log(x==y);
+console.log(x>=y);
+console.log(x<=y);
+console.log(x+=y);
+console.log(x-=y);
+console.log(x*=y);
+console.log(x/=y);
+
+// Logical Operator
+console.log(x&&=y);
+console.log(x||=y);
+console.log(x!=y);
+
+// Concatenation 
+function concatenateStrings(str1, str2) {
+    let str=str1+str2;
+    console.log(str)
+    
+}
+concatenateStrings("Hello","World!")
+
+
+function functionname(name){
+    console.log("Hello",name)
+}
+functionname("world")
+
+
+// Arthematic Operator
+let a=10;
+let b=5;
+
+let add=a+b;
+let sub=a-b;
+let mul=a*b;
+let div=a/b;
+let mod=a%b;
+let exp=a**b;
+
+console.log("Addition:"+add);
+console.log("Subtraction:"+sub);
+console.log("Multiplication:"+mul);
+console.log("Division:"+div);
+console.log("Modulus:"+mod);
+console.log("Exponentiation:"+exp);
+
+// Assignment Operators
+
+let equal = a = b;       // Assign b to a, then a's value is assigned to equal
+let plusequal = a += a;  // Adding a to itself (a += a is shorthand for a = a + a)
+let subequal = a -= a;   // Subtracting from a (a = a - a)
+let mulequal = a *= a;   // Multiplying a by itself (a *= a)
+
+console.log(equal);
+console.log(plusequal);
+console.log(subequal);
+console.log(mulequal);
+
+// Comparison Operator
+let comequal= a==b;
+let comStrictlyequal= a===b;
+let notequal= a!=b;
+let notStrictlyequal= a!==b;
+let greater= a>b;
+let less= a<b;
+let greaterequal= a>=b;
+let lessequal= a<=b;
+
+console.log(comequal);
+console.log(comStrictlyequal);
+console.log(notequal);
+console.log(notStrictlyequal);
+console.log(greater);
+console.log(less);
+console.log(greaterequal);
+console.log(lessequal);
+
+// Logical Operator
+let AND = a && b;
+let OR = a||b;
+
+console.log(AND);
+console.log(OR);
+
+// Predict the output
+console.log("5" - 2);           // 3
+console.log("5" + 2);           // 52
+console.log(true + 2);          // 3
+console.log(false == 0);        // true
+console.log(null == undefined); //true
