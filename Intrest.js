79da2ffe7be4c28a885ebe015f5ef7fd404a2b98
Harish-Kanagaraj <134590329+Harish-Kanagaraj@users.noreply.github.com>
2025-02-17 14:57:05 +0530

    Add files via upload

---
+++ Intrest.js
@@ -0,0 +1,19 @@
+const price = 100; 
+const taxRate = 0.08; 
+
+const total=price+(price*taxRate);
+console.log(total)
+
+//Print First 5 numbers
+
+for(let i=0;i<=5;i++){
+    console.log(i);
+}
+
+for(let i=5;i>=0;i--){
+    console.log(i)
+}
+
+
+
+   
