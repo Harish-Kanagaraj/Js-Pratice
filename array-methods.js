6ca587b76c1129630cf2ae9d9ef6399cc275e93e
Harish-Kanagaraj <134590329+Harish-Kanagaraj@users.noreply.github.com>
2025-02-19 12:04:47 +0530

    Create array-methods.js

---
+++ array-methods.js
@@ -0,0 +1,12 @@
+// Find Last Value
+const fruits=["Banana", "Orange", "Apple", "Mango"];
+let fruit=fruits[fruits.length - 1];
+console.log(fruit);
+
+// Find Array length
+let size = fruits.length;
+console.log(size);
+
+// Array to string
+let str=fruits.toString();
+console.log(str);
