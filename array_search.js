6c85ce4453e0804c2ea158b92bf5a101f8f6b557
Harish-Kanagaraj <134590329+Harish-Kanagaraj@users.noreply.github.com>
2025-03-11 21:17:12 +0530

    Update array_search.js

--- array_search.js
+++ array_search.js
@@ -3,3 +3,6 @@
  const fruits=["Apple", "Orange", "Apple", "Mango"];
  let position=fruits.indexOf("Orange")+1;
  console.log(position)
+ // last Index of() Method
+ let position1=fruits.lastIndexOf("Apple")+1;
+ console.log(position1);
