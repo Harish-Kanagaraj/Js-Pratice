af6b25b44a2f1685bf0d5acb93e21c28ee7f067f
Harish-Kanagaraj <134590329+Harish-Kanagaraj@users.noreply.github.com>
2025-03-12 07:15:23 +0530

    Update array_search.js

--- array_search.js
+++ array_search.js
@@ -6,3 +6,7 @@
  // last Index of() Method
  let position1=fruits.lastIndexOf("Apple")+1;
  console.log(position1);
+
+// includes()   -- check if an element is present
+ let position2=fruits.includes("Apple");
+ console.log(position2);
