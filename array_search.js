207cd33205328091d12e73d37792e823421344c0
Harish-Kanagaraj <134590329+Harish-Kanagaraj@users.noreply.github.com>
2025-03-14 20:51:02 +0530

    Update array_search.js

--- array_search.js
+++ array_search.js
@@ -19,3 +19,7 @@
  const array1 = [5, 12, 8, 130, 44];
  const found = array1.find((element) => element > 10);
  console.log(found);
+
+ // findIndex()
+ const isLargeNumber = (element) => element > 13;
+ console.log(array1.findIndex(isLargeNumber));
