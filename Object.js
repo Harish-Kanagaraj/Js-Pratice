84213bc590aea796f8e595e04f07c99dc7ae54ac
Harish-Kanagaraj <134590329+Harish-Kanagaraj@users.noreply.github.com>
2025-05-10 21:53:40 +0530

    Update Object.js

--- Object.js
+++ Object.js
@@ -165,3 +165,23 @@ let key1="name";
   age: 50,
   eyeColor: "blue"
 };
+
+
+    function getGrade(score) {
+    if(score===100){
+      console.log("A++");
+    }else if(score >= 90 && score <= 99){
+      console.log("A");
+    }else if(score >= 80 && score <= 89){
+      console.log("B");
+    }else if(score >= 70 && score <= 79){
+      console.log("C");
+    }else if(score >= 60 && score <= 69){
+      console.log("D");
+    }else if(score<=59){
+      console.log("F");
+    }
+    else{
+    console.log("Absent")
+    }
+    }
