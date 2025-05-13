22b4c3075321966431018846b205fcfe7a80370a
Harish-Kanagaraj <134590329+Harish-Kanagaraj@users.noreply.github.com>
2025-05-13 19:13:37 +0530

    Update Object.js

--- Object.js
+++ Object.js
@@ -205,3 +205,10 @@ for(let i=0;i<=5;i++){
 for(let i=5;i>=0;i--){
     console.log(i)
 }
+
+       function concatenateStrings(str1, str2) {
+    let str=str1+str2;
+    console.log(str)
+    
+}
+concatenateStrings("Hello","World!")
