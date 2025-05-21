1fe5c3aa73433e05382642f6fb502b5f8a9c49ed
Harish-Kanagaraj <134590329+Harish-Kanagaraj@users.noreply.github.com>
2025-05-21 23:01:22 +0530

    Update Object.js

--- Object.js
+++ Object.js
@@ -248,4 +248,16 @@ for(i=0;i<array.length;i++){
 console.log(sum);
 // Find highest number
 console.log(Math.max(...array));
+
+
+  function Person(first, last, age, eye) {
+   this.firstName = first;
+   this.lastName = last;
+   this.age = age;
+   this.eyeColor = eye;
+ }
  
+ 
+ const myFather = new Person("John", "Doe", 50, "blue");
+ document.getElementById("demo").innerHTML =
+ "My father is " + myFather.age + "."; 
