d7d016aa7d6ab6fb02e5c5bb7049f2aa6ead4985
Harish-Kanagaraj <134590329+Harish-Kanagaraj@users.noreply.github.com>
2025-05-24 22:01:43 +0530

    Update add array.js

--- add array.js
+++ add array.js
@@ -10,3 +10,26 @@ console.log(Math.max(...array));
 const fruits=["Banana", "Orange", "Apple", "Mango"];
 let fruit=fruits[fruits.length - 1];
 console.log(fruit);
+
+ fetch('https://jsonplaceholder.typicode.com/todos',{
+     method:'POST',
+     headers:{'content-type':'application/json'},
+     body:JSON.stringify({
+       id:1,
+       title:"Lourem",
+       userId:11,
+       completed:false
+       })
+     })
+     .then(response=>response.json())
+     .then(json=>console.log(json))
+     
+ 
+ fetch('https://jsonplaceholder.typicode.com/todos')
+   .then(response => response.json())
+   .then(json => {
+     json.forEach(todo => {
+       document.write(`<p>Title: ${todo.title} </p>`);
+     });
+   })
+   .catch(error => console.error('Error fetching data:', error));
