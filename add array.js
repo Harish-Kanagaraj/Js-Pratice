bb1d6f0e45447fdd912f5141f4271ec738885559
Harish-Kanagaraj <134590329+Harish-Kanagaraj@users.noreply.github.com>
2025-05-30 22:06:05 +0530

    Update add array.js

--- add array.js
+++ add array.js
@@ -39,3 +39,9 @@ for(i=0;i<array.length;i++){
     sum+=array[i];
 }
 console.log(sum);
+let array=[10,25,35,100];
+let sum=0;
+for(i=0;i<array.length;i++){
+    sum+=array[i];
+}
+console.log(sum);
