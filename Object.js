a289c4695742c1ead9bc5ac00514fbf9e3b88ba9
Harish-Kanagaraj <134590329+Harish-Kanagaraj@users.noreply.github.com>
2025-05-31 23:01:47 +0530

    Update Object.js

--- Object.js
+++ Object.js
@@ -273,3 +273,8 @@ fetch('https://jsonplaceholder.typicode.com/todos',{
      })
      .then(response=>response.json())
      .then(json=>console.log(json))
+
+let key = prompt("What do you want to know about the user?", "name");
+ alert(users[key]);
+let key1="name";
+ alert( users.key1 )
