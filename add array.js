2c732731346cb0ec2eb36dbe331a72af4c38d40c
Harish-Kanagaraj <134590329+Harish-Kanagaraj@users.noreply.github.com>
2025-06-08 22:43:06 +0530

    Update add array.js

--- add array.js
+++ add array.js
@@ -57,3 +57,9 @@ fetch('https://jsonplaceholder.typicode.com/todos')
        document.write(`<p>Title: ${todo.title} </p>`);
      });
    })
+let array=[10,25,35,100];
+let sum=0;
+for(i=0;i<array.length;i++){
+    sum+=array[i];
+}
+console.log(sum);
