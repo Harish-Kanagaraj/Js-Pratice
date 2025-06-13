3a9e09f5ca968bd6e98fe69f1ba0a5ac52c8c270
Harish-Kanagaraj <134590329+Harish-Kanagaraj@users.noreply.github.com>
2025-06-13 23:45:17 +0530

    Update Object.js

--- Object.js
+++ Object.js
@@ -318,3 +318,8 @@ let key = prompt("What do you want to know about the user?", "name");
  alert(users[key]);
 let key1="name";
  alert( users.key1 )
+
+let pos=fruits.at(2);
+let pos1 = fruits[2];
+console.log(pos);
+console.log(pos1);
